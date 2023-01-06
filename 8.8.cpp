d609b09539754fed47639906ed1bce34e59acd3f
Kiryl <katana96@mail.ru>
2023-01-06 15:49:55 +0300

    Exercise8.8

--- 8.8.cpp
+++ 8.8.cpp
@@ -16,9 +16,34 @@ int main()
     std::cout << std::endl;
 
     vPtr = values;
-    std::cout << *vPtr;
+    std::cout << "==========================="<<std::endl;
 
-    //e 
+    //e) вывод элементов массива используя нотации Указатель/смещение
+
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << *(vPtr+i) <<" ";
+    }
+
+    std::cout << std::endl << "==========================="<<std::endl;
+    //f)
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << *(values+i) <<" ";
+    }
+
+    std::cout << std::endl << "==========================="<<std::endl;
+
+    //g) вывод элементов массива используя индекс с указателем на массив
+
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << vPtr[i] <<" ";
+    }
+
+    std::cout << std::endl << "==========================="<<std::endl;
+
+    // h)
+    std::cout<< "Five element array Value = " << *(values+Size-1) <<" ";
     
-
 }
